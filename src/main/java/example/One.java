502b0edc0d93b20debdcdc8ce0476839f9bc2724
jdcoffre <jeandenis.coffre@sonarsource.com>
2017-02-02 07:55:09 +0100

    init

---
+++ src/main/java/example/One.java
@@ -0,0 +1,25 @@
+package example;
+
+public class One {
+  String message = "foo";
+  String message2 = "toto";
+
+  public String foo() {
+    return message;
+  }
+
+  public String toto() {
+    return message2;
+  }
+
+  public String titi() {
+    System.out.println("Adding a line");
+    System.out.println("Adding a line");
+    System.out.println("Adding a line");
+    return message2;
+  }
+
+  public void uncoveredMethod() {
+    System.out.println(foo());
+  }
+}
---
+++ src/test/java/example/OneTest.java
@@ -0,0 +1,24 @@
+package example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class OneTest {
+  @Test
+  public void testFoo() throws Exception {
+    One one = new One();
+    assertEquals("foo", one.foo());
+  }
+  @Test
+  public void testTiti() throws Exception {
+    One one = new One();
+    assertEquals("toto", one.titi());
+  }
+
+  @Test
+  public void testBoth() throws Exception {
+    One one = new One();
+    assertEquals("toto", one.toto());
+    assertEquals("foo", one.foo());
+  }
+}
